240783388b6c7b4ea6a0b28b6c1233e34d10fcec
Spidy092 <Swaragh092@gmail.com>
2026-06-02 18:57:58 +0530

    commond cli

--- open-sse/executors/index.js
+++ open-sse/executors/index.js
@@ -15,6 +15,7 @@ import { GrokWebExecutor } from "./grok-web.js";
 import { PerplexityWebExecutor } from "./perplexity-web.js";
 import { OllamaLocalExecutor } from "./ollama-local.js";
 import { CommandCodeExecutor } from "./commandcode.js";
+import { CommandCodeCLIExecutor } from "./commandcode-cli.js";
 import { DefaultExecutor } from "./default.js";
 
 const executors = {
@@ -37,6 +38,7 @@ const executors = {
   "perplexity-web": new PerplexityWebExecutor(),
   "ollama-local": new OllamaLocalExecutor(),
   commandcode: new CommandCodeExecutor(),
+  "commandcode-cli": new CommandCodeCLIExecutor(),
 };
 
 const defaultCache = new Map();
@@ -70,3 +72,4 @@ export { GrokWebExecutor } from "./grok-web.js";
 export { PerplexityWebExecutor } from "./perplexity-web.js";
 export { OllamaLocalExecutor } from "./ollama-local.js";
 export { CommandCodeExecutor } from "./commandcode.js";
+export { CommandCodeCLIExecutor } from "./commandcode-cli.js";
